217ae724603b30b425246774de8259383b8fa93a
LlaGuiTo <santiago.losada.borrajo@gmail.com>
2026-07-07 18:14:36 +0200

    v1.0.6: Iron chestplate icon on cosmetic button

--- src/main/java/com/skd/armorcosmetic/impl/client/gui/GuiCosArmorButton.java
+++ src/main/java/com/skd/armorcosmetic/impl/client/gui/GuiCosArmorButton.java
@@ -4,15 +4,20 @@ import java.util.function.BiConsumer;
 
 import net.minecraft.client.gui.GuiGraphicsExtractor;
 import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.renderer.RenderPipelines;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.Identifier;
 
 public class GuiCosArmorButton extends Button implements IShiftingWidget, ICreativeInvWidget {
 
+    private static final Identifier IRON_CHESTPLATE = Identifier.withDefaultNamespace("textures/item/iron_chestplate.png");
     private final BiConsumer<GuiCosArmorButton, Boolean> onCreativeTabChanged;
+    private final boolean showIcon;
 
-    public GuiCosArmorButton(int x, int y, int width, int height, Component message,
+    public GuiCosArmorButton(int x, int y, int width, int height, Component message, boolean showIcon,
             OnPress onPress, BiConsumer<GuiCosArmorButton, Boolean> onCreativeTabChanged) {
         super(x, y, width, height, message, onPress, DEFAULT_NARRATION);
+        this.showIcon = showIcon;
         this.onCreativeTabChanged = onCreativeTabChanged;
     }
 
@@ -21,7 +26,9 @@ public class GuiCosArmorButton extends Button implements IShiftingWidget, ICreat
         int bgColor = isHoveredOrFocused() ? 0xFFFFFFFF : 0xFF8B8B8B;
         graphics.fill(getX(), getY(), getX() + width, getY() + height, 0xFF000000);
         graphics.fill(getX() + 1, getY() + 1, getX() + width - 1, getY() + height - 1, bgColor);
-        if (width >= 14) {
+        if (showIcon && width >= 16 && height >= 16) {
+            graphics.blit(RenderPipelines.GUI_TEXTURED, IRON_CHESTPLATE, getX() + (width - 16) / 2, getY() + (height - 16) / 2, 0, 0, 16, 16, 16, 16);
+        } else if (width >= 14) {
             graphics.centeredText(
                     net.minecraft.client.Minecraft.getInstance().font,
                     getMessage(),
